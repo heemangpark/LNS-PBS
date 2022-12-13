0b68438ef4a446892565c3efcccee9781c214de1
cu-rie <ahnkjuree@gmail.com>
2022-12-13 12:48:51 +0900

    refcator

--- main_eecbs.py
+++ main_eecbs.py
@@ -1,15 +1,42 @@
 import os
 import subprocess
 import sys
+from utils.solver_util import save_map, save_scenario
+from utils.generate_scenarios import load_scenarios, save_scenarios
+
+save_scenarios(M=10, N=10)
+scenario = load_scenarios('./scenarios/323220_1_10_10/scenario_2.pkl')
+# scenario = load_scenarios('./instance_scenarios/16_16_0.2/scenario_1.pkl')
+grid, graph, agent_pos, total_tasks = scenario[0], scenario[1], scenario[2], scenario[3]
+
+directory = 'data/'
+scenario_name = 'mapfile'
+
+save_map(grid, directory + scenario_name)
+save_scenario(agent_pos, total_tasks, directory+scenario_name, grid.shape[0], grid.shape[1])
 
 sys.path.append(os.path.abspath('EECBS'))
 
 solver_path = "EECBS/"
 c = [solver_path + "eecbs",
-     "-m", solver_path + "random-32-32-20.map",
-     "-a", solver_path + "random-32-32-20-random-1.scen",
-     "-o", solver_path + "test.csv",
+     "-m",
+     # solver_path + "random-32-32-20.map",
+     directory + scenario_name + '.map',
+     "-a",
+     # solver_path + "random-32-32-20-random-1.scen",
+     directory + scenario_name + '.scen',
+     "-o",
+     solver_path + "test.csv",
      "--outputPaths=paths.txt",
      "-k", "50", "-t", "60", "--suboptimality=1.2"]
 
 subprocess.run(c)
+
+# def invoke_EECBS(cmd):
+#     subprocess.Popen(cmd)
+#
+#
+# if __name__ == '__main__':
+#     cmd = "./eecbs -m random-32-32-20.map -a random-32-32-20-random-1.scen -o test.csv --outputPaths=paths.txt -k 50 -t 60 --suboptimality=1.2 "
+#     print('A')
+#     invoke_EECBS(cmd)
---
+++ utils/solver_util.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def save_map(grid, filename):
+    f = open('{}.map'.format(filename), 'w')
+    f.write('type four-directional\n')
+    f.write('height {}\n'.format(grid.shape[0]))
+    f.write('width {}\n'.format(grid.shape[1]))
+    f.write('map\n')
+
+    # creating map from grid
+    map_dict = {0: '.', 1: '@'}
+    for r in range(grid.shape[0]):
+        line = grid[r]
+        l = []
+        for g in line:
+            l.append(map_dict[g])
+        f.write(''.join(l) + '\n')
+
+    f.close()
+
+
+def save_scenario(agent_pos, total_tasks, scenario_name, row, column):
+    f = open('{}.scen'.format(scenario_name), 'w')
+    f.write('version 1\n')
+    for a, t in zip(agent_pos, total_tasks):
+        task = t[0]  # TODO:add task seq
+        dist = abs(np.array(a) - np.array(t)).sum()  # Manhatten dist
+        line = '1 \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t{}'.format('temp.scen', row, column, a[1], a[0], task[1],
+                                                                  task[0], dist)
+        f.write(line + "\n")
+    f.close()
