155c5d969496ca15b0ffcf56d3ce90b6c1e3984c
cu-rie <ahnkjuree@gmail.com>
2022-12-22 11:54:35 +0900

    scenario-path

--- main_eecbs.py
+++ main_eecbs.py
@@ -1,36 +1,34 @@
 import subprocess
+import os
 
-from utils.generate_scenarios import load_scenarios
+from utils.generate_scenarios import load_scenarios, save_scenarios
 from utils.solver_util import save_map, save_scenario
 from utils.vis_graph import vis_dist
 
-scenario = load_scenarios('323220_1_20_50/scenario_1.pkl')
+M, N = 50, 50
+if not os.path.exists('scenarios/323220_1_{}_{}/'.format(N, M)):
+    save_scenarios(size=32, M=M, N=N)
+
+scenario = load_scenarios('323220_1_{}_{}/scenario_1.pkl'.format(N, M))
 grid, graph, agent_pos, total_tasks = scenario[0], scenario[1], scenario[2], scenario[3]
 vis_dist(graph, agent_pos, total_tasks)
 
-scenario_name = 'test'
+scenario_name = 'test1'
 save_map(grid, scenario_name)
 save_scenario(agent_pos, total_tasks, scenario_name, grid.shape[0], grid.shape[1])
 
 solver_path = "EECBS/"
 c = [solver_path + "eecbs",
-     # "-m", solver_path + "random-32-32-20.map",
-     "-m", solver_path + scenario_name + '.map',
-     # "-a", solver_path + "random-32-32-20-random-1.scen",
-     "-a", solver_path + scenario_name + '.scen',
-     "-o", solver_path + "test.csv",
-     "--outputPaths = EECBS/paths.txt",
-     "-k", "{}".format(20),
-     "-t", "60",
-     "--suboptimality = 1.2"]
+     "-m",
+     solver_path + scenario_name + '.map',
+     "-a",
+     solver_path + scenario_name + '.scen',
+     "-o",
+     solver_path + scenario_name + ".csv",
+     "--outputPaths",
+     solver_path + scenario_name + "_paths.txt",
+     "-k", "{}".format(M), "-t", "60", "--suboptimality=1.2"]
 
 subprocess.run(c)
 
-# def invoke_EECBS(cmd):
-#     subprocess.Popen(cmd)
-#
-#
-# if __name__ == '__main__':
-#     cmd = "./eecbs -m random-32-32-20.map -a random-32-32-20-random-1.scen -o test.csv --outputPaths=paths.txt -k 50 -t 60 --suboptimality=1.2 "
-#     print('A')
-#     invoke_EECBS(cmd)
+f = open(solver_path + scenario_name + "_paths.txt", 'r')
