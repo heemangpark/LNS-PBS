85d835e77019547d62a119f3d06eefdb562dddfc
heemangpark <heemang0826@kaist.ac.kr>
2022-12-22 20:57:40 +0900

    oracle dataset

--- main_eecbs.py
+++ main_eecbs.py
@@ -1,17 +1,17 @@
-import subprocess
 import os
+import subprocess
 
+from nn.agent import Agent
 from utils.generate_scenarios import load_scenarios, save_scenarios
 from utils.solver_util import save_map, save_scenario, read_trajectory
 from utils.vis_graph import vis_dist
-from nn.agent import Agent
 
 solver_path = "EECBS/"
 M, N = 10, 10
-if not os.path.exists('scenarios/323220_1_{}_{}/'.format(N, M)):
+if not os.path.exists('scenarios/323220_1_{}_{}/'.format(M, N)):
     save_scenarios(size=32, M=M, N=N)
 
-scenario = load_scenarios('323220_1_{}_{}/scenario_1.pkl'.format(N, M))
+scenario = load_scenarios('323220_1_{}_{}/scenario_1.pkl'.format(M, N))
 grid, graph, agent_pos, total_tasks = scenario[0], scenario[1], scenario[2], scenario[3]
 vis_dist(graph, agent_pos, total_tasks)
 
@@ -20,6 +20,8 @@ save_map(grid, scenario_name)
 total_tasks_bef = total_tasks
 
 ag = Agent()
+
+# TODO total_tasks format: 태스크 전체 (순서대로) 쭉 나열된 일종의 task 집합 -> 'agent to task' 할당 집합으로 바꿔야 함 (main.py에서 tasks에 해당)
 
 for i in range(10):
     save_scenario(agent_pos, total_tasks_bef, scenario_name, grid.shape[0], grid.shape[1])
---
+++ nn/oracle.py
@@ -0,0 +1,39 @@
+import pickle
+
+from LNS.hungarian import hungarian
+from LNS.regret import f_ijk, get_regret
+from LNS.shaw import removal
+from utils.generate_scenarios import save_scenarios, load_scenarios
+
+"""
+323220 / C=1 / M=10 / N=10 setting
+"""
+
+save_scenarios(itr=1000)
+for idx in list(range(1, 1001)):
+    scenario = load_scenarios('323220_1_10_10/scenario_{}.pkl'.format(idx))
+    grid, graph, agent_pos, total_tasks = scenario[0], scenario[1], scenario[2], scenario[3]
+    task_idx, tasks = hungarian(graph, agent_pos, total_tasks)
+
+    for _ in range(500):  # 10 10 setting 500 itrs might be enough / not sure :(
+        removal_idx = removal(task_idx, total_tasks, graph)
+        for i, t in enumerate(tasks.values()):
+            for r in removal_idx:
+                if {r: total_tasks[r]} in t:
+                    tasks[i].remove({r: total_tasks[r]})
+        while len(removal_idx) != 0:
+            "time consuming"
+            f = f_ijk(tasks, agent_pos, removal_idx, total_tasks, graph)
+            regret = get_regret(f)
+            regret = dict(sorted(regret.items(), key=lambda x: x[1][0], reverse=True))
+            re_ins = list(regret.keys())[0]
+            re_a, re_j = regret[re_ins][1], regret[re_ins][2]
+            removal_idx.remove(re_ins)
+            to_insert = {re_ins: total_tasks[re_ins]}
+            tasks[re_a].insert(re_j, to_insert)
+
+    with open('data/1010AP_{}'.format(idx), 'wb') as f:  # agent position
+        pickle.dump(agent_pos, f)
+
+    with open('data/1010TA_{}'.format(idx), 'wb') as f:  # tasks assignments
+        pickle.dump(tasks, f)
