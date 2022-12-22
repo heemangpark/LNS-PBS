79154e82b240ba0d01b0d159142f7dcb21c02d6f
cu-rie <ahnkjuree@gmail.com>
2022-12-22 17:33:00 +0900

    .

--- main_eecbs.py
+++ main_eecbs.py
@@ -5,6 +5,7 @@ from utils.generate_scenarios import load_scenarios, save_scenarios
 from utils.solver_util import save_map, save_scenario, read_trajectory
 from utils.vis_graph import vis_dist
 from nn.agent import Agent
+
 solver_path = "EECBS/"
 M, N = 10, 10
 if not os.path.exists('scenarios/323220_1_{}_{}/'.format(N, M)):
@@ -21,22 +22,22 @@ total_tasks_bef = total_tasks
 ag = Agent()
 
 for i in range(10):
-     save_scenario(agent_pos, total_tasks_bef, scenario_name, grid.shape[0], grid.shape[1])
-     c = [solver_path + "eecbs",
-          "-m",
-          solver_path + scenario_name + '.map',
-          "-a",
-          solver_path + scenario_name + '.scen',
-          "-o",
-          solver_path + scenario_name + ".csv",
-          "--outputPaths",
-          solver_path + scenario_name + "_paths.txt",
-          "-k", "{}".format(M), "-t", "60", "--suboptimality=1.2"]
-     subprocess.run(c)
-     agent_traj = read_trajectory(solver_path + scenario_name + "_paths.txt")
-     costs = [len(t) for t in agent_traj]
+    save_scenario(agent_pos, total_tasks_bef, scenario_name, grid.shape[0], grid.shape[1])
+    c = [solver_path + "eecbs",
+         "-m",
+         solver_path + scenario_name + '.map',
+         "-a",
+         solver_path + scenario_name + '.scen',
+         "-o",
+         solver_path + scenario_name + ".csv",
+         "--outputPaths",
+         solver_path + scenario_name + "_paths.txt",
+         "-k", "{}".format(M), "-t", "60", "--suboptimality=1.2"]
+    subprocess.run(c)
+    agent_traj = read_trajectory(solver_path + scenario_name + "_paths.txt")
+    costs = [len(t) for t in agent_traj]
 
-     total_tasks_after = ag(agent_pos, total_tasks_bef)
-     total_tasks_bef = total_tasks_after
+    total_tasks_after = ag(graph, agent_pos, total_tasks_bef)
+    total_tasks_bef = total_tasks_after
 
-print()
+    print("cost:{}".format(sum(costs)))
