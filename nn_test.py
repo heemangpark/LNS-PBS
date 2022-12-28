0b02d8a957d9ad3d4a197833d3c10b532a70698a
cu-rie <ahnkjuree@gmail.com>
2022-12-28 16:01:42 +0900

    agent goes to the empty node

--- nn_test.py
+++ nn_test.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import numpy as np
 
 from nn.agent import Agent
 from nn.ag_util import convert_dgl
@@ -18,7 +19,6 @@ vis_dist(graph, agent_pos, total_tasks)
 
 scenario_name = 'test1'
 save_map(grid, scenario_name)
-total_tasks_bef = total_tasks
 
 ag = Agent()
 
@@ -28,25 +28,35 @@ TODO total_tasks format
 """
 itr = 0
 
-di_dgl_g, ag_node_indices, task_node_indices = convert_dgl(graph, agent_pos, total_tasks_bef, [])
-selected_task = [False for _ in range(N)]
-while True:
-    if itr == 0:
+agent_traj = []
+task_finished = [False for _ in range(N)]
+di_dgl_g, ag_node_indices, task_node_indices = convert_dgl(graph, agent_pos, total_tasks, agent_traj)
+joint_action = []
+
+while not all(task_finished):
+    if itr != 100:
+        task_selected = [False for _ in range(N)]
         total_tasks_bef = []
+        # TODO: agent sequence
         for ag_idx in ag_node_indices:
-            action = ag(di_dgl_g, ag_idx, task_node_indices, selected_task)
+            action = ag(di_dgl_g, ag_idx, task_node_indices, task_selected)
             task_node_idx = task_node_indices[action]
 
             # convert action to executable format (task pos)
             _x = di_dgl_g.nodes[task_node_idx].data['x'].item()
             _y = di_dgl_g.nodes[task_node_idx].data['y'].item()
-            total_tasks_bef.append([_x, _y])
-            selected_task[action] = True
+            total_tasks_bef.append([[_x, _y]])
+            task_selected[action] = True
+
+            joint_action.append(action)
     else:
         raise NotImplementedError
         total_tasks_bef = []
 
+    # convert action to the solver input formation
     save_scenario(agent_pos, total_tasks_bef, scenario_name, grid.shape[0], grid.shape[1])
+
+    # Run solver
     # c = [solver_path + "eecbs",
     #      "-m",
     #      solver_path + scenario_name + '.map',
@@ -58,14 +68,24 @@ while True:
     #      solver_path + scenario_name + "_paths.txt",
     #      "-k", "{}".format(M), "-t", "60", "--suboptimality=1.2"]
     # subprocess.run(c)
-    # agent_traj = read_trajectory(solver_path + scenario_name + "_paths.txt")
 
-    # Agent position 을 index 로 저장
+    # Read solver output
+    agent_traj = read_trajectory(solver_path + scenario_name + "_paths.txt")
+
+    # Mark finished agent, finished task
+    finished_ag_idx = np.argmin([len(t) for t in agent_traj])
+    finished_task_idx = joint_action[finished_ag_idx]
+    task_finished[finished_task_idx] = True
+
+    # overwrite output
+    next_t = len(agent_traj[finished_ag_idx])
+    agent_pos = [t[next_t - 1] for t in agent_traj]
+
+    # Replay memory에 transition 저장. Agent position을 graph의 node 형태로
+    # TODO
     agent_traj = []
 
     di_dgl_g, ag_node_indices, task_node_indices = convert_dgl(graph, agent_pos, total_tasks_bef, agent_traj)
-    total_tasks_after = ag(di_dgl_g, ag_node_indices[0], task_node_indices)
-    total_tasks_bef = total_tasks_after
 
     costs = [len(t) for t in agent_traj]
     print("itr:{}, cost:{}".format(itr, sum(costs)))
