acd7fd77db0838e7261838451c189b3f30fee815
cu-rie <ahnkjuree@gmail.com>
2023-01-02 16:51:57 +0900

    read-solver-out

--- nn_test.py
+++ nn_test.py
@@ -10,7 +10,7 @@ from utils.vis_graph import vis_dist, vis_ta
 from copy import deepcopy
 
 solver_path = "EECBS/"
-M, N = 5, 5
+M, N = 10, 10
 # if not os.path.exists('scenarios/323220_1_{}_{}/'.format(M, N)):
 # save_scenarios(size=32, M=M, N=N)
 
@@ -36,39 +36,32 @@ di_dgl_g, ag_node_indices, task_node_indices = convert_dgl(graph, agent_pos, tot
 joint_action = []
 
 while not all(task_finished):
-    print("ITR:{}".format(itr))
-    if itr != 100:
-        # `task_selected` initialized as the `task_finished` to jointly select task at each event
-        task_selected = deepcopy(task_finished)
-        total_tasks_solver = []
-        agent_pos_solver = []
-        joint_action = []
-        # TODO: agent sequence
-        for i, ag_node_idx in enumerate(ag_node_indices):
-            if not all(task_selected):
-                # index of task. action \in [1, ..., N]
-                action = ag(di_dgl_g, ag_node_idx, task_node_indices, task_selected)
-                task_node_idx = task_node_indices[action]
+    # `task_selected` initialized as the `task_finished` to jointly select task at each event
+    task_selected = deepcopy(task_finished)
+    total_tasks_solver = []
+    agent_pos_solver = []
+    joint_action = []
+    # TODO: How to decide agent sequence? Currently by agent index
+    for i, ag_node_idx in enumerate(ag_node_indices):
+        if not all(task_selected):
+            # index of task. action \in [1, ..., N]
+            action = ag(di_dgl_g, ag_node_idx, task_node_indices, task_selected)
+            task_node_idx = task_node_indices[action]
 
-                # convert action to executable format (task pos)
-                _x = di_dgl_g.nodes[task_node_idx].data['x'].item()
-                _y = di_dgl_g.nodes[task_node_idx].data['y'].item()
-                total_tasks_solver.append([[_x, _y]])
-                agent_pos_solver.append(agent_pos[i])
-                task_selected[action] = True
-            else:
-                action = None
-                # maintain current position
-                # total_tasks_bef.append([agent_pos[i]])
-                # total_tasks_bef.append([agent_pos[i]])
+            # convert action to solver format (task pos)
+            _x = di_dgl_g.nodes[task_node_idx].data['x'].item()
+            _y = di_dgl_g.nodes[task_node_idx].data['y'].item()
+            total_tasks_solver.append([[_x, _y]])
+            agent_pos_solver.append(agent_pos[i])
+            task_selected[action] = True
+        else:
+            action = None
+            # maintain current position
 
-            joint_action.append(action)
-    else:
-        raise NotImplementedError
-        total_tasks_bef = []
+        joint_action.append(action)
 
     # visualize
-    vis_ta(graph, agent_pos_solver, total_tasks_solver, str(itr)+"_assigned")
+    vis_ta(graph, agent_pos_solver, total_tasks_solver, str(itr) + "_assigned")
 
     # convert action to the solver input formation
     save_scenario(agent_pos_solver, total_tasks_solver, scenario_name, grid.shape[0], grid.shape[1])
@@ -85,7 +78,12 @@ while not all(task_finished):
          "--outputPaths",
          solver_path + scenario_name + "_paths.txt",
          "-k", "{}".format(len(agent_pos_solver)), "-t", "60", "--suboptimality=1.1"]
-    subprocess.run(c)
+
+    # process_out.stdout format
+    # runtime, num_restarts, num_expanded, num_generated, solution_cost, min_sum_of_costs, avg_path_length
+    process_out = subprocess.run(c, capture_output=True)
+    text_byte = process_out.stdout.decode('utf-8')
+    sum_costs = text_byte.split('Succeed,')[-1].split(',')[-3]
 
     # Read solver output
     agent_traj = read_trajectory(solver_path + scenario_name + "_paths.txt")
@@ -100,13 +98,14 @@ while not all(task_finished):
     agent_pos = [traj[next_t - 1] for traj in agent_traj]
 
     # Replay memory 에 transition 저장. Agent position 을 graph 의 node 형태로
+    # NOTE: solver out cost == 아래의 cost - M
+    # costs = [len(t) for t in agent_traj]
+    print("itr:{}, cost:{}".format(itr, sum_costs))
     # TODO
     agent_traj = []
 
     di_dgl_g, ag_node_indices, _ = convert_dgl(graph, agent_pos, total_tasks, agent_traj)
 
-    costs = [len(t) for t in agent_traj]
-    print("itr:{}, cost:{}".format(itr, sum(costs)))
     # visualize
-    vis_ta(graph, agent_pos, total_tasks_solver, str(itr)+"_finished")
+    vis_ta(graph, agent_pos, total_tasks_solver, str(itr) + "_finished")
     itr += 1
