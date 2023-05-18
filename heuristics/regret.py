18c7de820ddd7a008ef34fc26f42f2827be9f3ef
cu-rie <ahnkjuree@gmail.com>
2023-05-18 10:24:51 +0900

    f_ijk_done

--- heuristics/regret.py
+++ heuristics/regret.py
@@ -1,9 +1,10 @@
-from copy import deepcopy
-from functools import partial
-
 import numpy as np
+from copy import deepcopy
+# from functools import partial
+from math import inf
+# from utils.astar import graph_astar
 
-from utils.astar import graph_astar
+
 
 
 def manhattan(coord_1, coord_2, *args):
@@ -12,57 +13,47 @@ def manhattan(coord_1, coord_2, *args):
     return x + y
 
 
-def f_ijk(current_tasks, agent_pos, removal_idx, total_tasks, graph, metric='man'):
+def f_ijk(current_tasks, agent_pos, removal_indices, total_tasks, graph, metric='man'):
     assert metric == 'man', "else than man not implemented yet. " \
                             "Check commit 0f816c10240a59fa8f2edef3f925206aead4c7bd to revert "
     n_ag = len(agent_pos)
-    before_cost = []
-    before_edge_cost = [[] for _ in range(n_ag)]
+    total_tasks = np.array(total_tasks)
+    before_all_locs = []
 
+    # before location
     for ag_idx in range(n_ag):
         schedule = current_tasks[ag_idx]
-        if len(schedule) == 0:
-            before_edge_cost[ag_idx].append(0)
-            before_cost.append(0)
-            continue
-        task_locs = np.array(total_tasks)[schedule]
+        task_locs = total_tasks[schedule]
         all_locs = np.concatenate([agent_pos[ag_idx].reshape(1, -1), task_locs])
-        all_edge_costs = np.abs(all_locs[:-1] - all_locs[1:]).sum(-1)
+        before_all_locs.append(all_locs)
 
-        before_edge_cost[ag_idx].append(all_edge_costs)
-        before_cost.append(all_edge_costs.sum())
+    before_all_locs = np.concatenate(before_all_locs)
+    before_edge_cost = manhattan_edgewise(before_all_locs)
 
-    f = dict(zip(removal_idx, [list() for _ in range(len(removal_idx))]))
-    for i in removal_idx:
-        rt = total_tasks[i]
-        for k in range(n_ag):
-            ats = current_tasks[k]
-            f_list = list()
-            for j in range(len(ats) + 1):
-                temp = deepcopy(ats)
-                path = list()
-                for a in temp:
-                    for task_pos in a.values():
-                        path += task_pos
+    # insertion
+    ret_f = dict()
+    for removal_idx in removal_indices:
+        removal_pos = np.array(total_tasks[removal_idx])
+        insert_src = before_all_locs[..., :-1, :]
+        insert_dst = before_all_locs[..., 1:, :]
+        removal_pos_r = np.tile(removal_pos, (*insert_src.shape[:-1], 1))
 
-                if j == 0:
-                    f_value = sum(before_edge_cost[k][1:])
-                    f_value += method(agent_pos[k], rt[0], graph)
-                    f_value += method(rt[0], path[0], graph)
+        # inserted_edges = (s, removal_node, d)
+        inserted_edges = np.stack([insert_src, removal_pos_r, insert_dst], axis=-2)  # n_nodes, 3, 2
+        inserted_edge_cost = manhattan_edgewise(inserted_edges).sum(-1)
 
-                elif j == len(ats):
-                    f_value = sum(before_edge_cost[k])
-                    f_value += method(path[-1], rt[0], graph)
+        # to mask out schedule-to-schedule position, identify whether the destination location is agent position or not
+        redundant_edge = insert_dst.reshape(-1, 1, 2) == agent_pos.reshape(1, -1, 2)
+        agent_indicator = redundant_edge.prod(-1)  # (x1 == x2) and (y1 == y2)
+        is_redundant = agent_indicator.sum(-1).astype(bool)  # 1 if the redundant edge
 
-                else:
-                    f_value = sum(before_edge_cost[k]) - before_edge_cost[k][j]
-                    f_value += method(path[j - 1], rt[0], graph)
-                    f_value += method(rt[0], path[j], graph)
+        edge_cost_gap = inserted_edge_cost - before_edge_cost
+        edge_cost_gap = edge_cost_gap.astype(float)
+        edge_cost_gap[is_redundant] = inf
 
-                f_list.append(f_value)
-            f[i].append(f_list)
+        ret_f[removal_idx] = edge_cost_gap
 
-    return f
+    return ret_f
 
 
 def get_regret(f_values):
@@ -81,3 +72,12 @@ def get_regret(f_values):
         regret[removal_idx] = [v[1] - v[0], a, j]
 
     return regret
+
+
+def manhattan_edgewise(positions):
+    if type(positions) == list:
+        positions = np.array(positions)
+
+    src_pos = positions[..., :-1, :]
+    dst_pos = positions[..., 1:, :]
+    return np.abs(src_pos - dst_pos).sum(-1)
